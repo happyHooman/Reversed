6a752b646d185f91942af0f4ff08a007dd4e4f14
MihaiCataPop <mihaicatapop@Gmail.com>
2017-10-31 21:21:09 +0200

    Load data from JSON. Correct order of the players.

--- js/functionality.js
+++ js/functionality.js
@@ -8,22 +8,54 @@
 //
 //
 // }
+var nextPlayer = 1;
 
-var list = [];
-for(var i=0; i<8; i++){
-    list.push('<tr>');
-    for(var j=0; j<8; j++) {
-        var colorata = i % 2 ? j % 2 : ((j+1) % 2);
-        list.push('<td class="square ' + (colorata ? 'color' : '')+ ' "></td>');
+function generateEmptyTable() {
+    var list = [];
+    for (var i = 0; i < 8; i++) {
+        list.push('<tr>');
+        for (var j = 0; j < 8; j++) {
+            var colorata = i % 2 ? j % 2 : ((j + 1) % 2);
+            list.push('<td class="square ' + (colorata ? 'color' : '') + ' "></td>');
+        }
+        list.push('</tr>');
     }
-    list.push('</tr>');
+    $('#mainBoard').html(list.join(''));
 }
-$('#mainBoard').html(list.join(''));
 
-
-
-    $("#mainBoard").on("click", "td", function(){
-        $(this).append('<div id="piesa"></div>');
+function load() {
+    $.ajax('date.json', {
+        cache: false,
+        dataType: 'json'
+    }).done(function (raspuns) {
+        console.debug('contacts loaded', raspuns);
+        var status = raspuns.table;
+        nextPlayer = raspuns.nextPlayer;
+        for (var i = 0; i < 8; i++) {
+            for (var j = 0; j < 8; j++) {
+                var piesa = status [i][j];
+                if (piesa){
+                    var td = $('#mainBoard tr').eq(i).find('td').eq(j);
+                    td.append('<div class="piesa player' + piesa +'"></div>');
+                }
+            }
+        }
     });
 
-//comment
+}
+
+generateEmptyTable();
+load();
+
+$("#mainBoard").on("click", "td", function () {
+    var td = $(this);
+    if (td.find('.piesa').length == 0) {
+        td.append('<div class="piesa player' + nextPlayer + '"></div>');
+        if (nextPlayer == 1) {
+            nextPlayer = 2
+        } else if (nextPlayer == 2) {
+            nextPlayer = 1
+        }
+    }
+});
+
