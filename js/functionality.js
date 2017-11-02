ea2ca5bdcfec2eaf408d6ba428bf5c380c86e28e
HappyHooman <tornea.alexandru@gmail.com>
2017-11-02 12:03:39 +0200

    code formatting find the coordinates of the clicked cell find enemy neighbours

--- js/functionality.js
+++ js/functionality.js
@@ -8,13 +8,19 @@
 //
 //
 // }
+
 var nextPlayer = 1;
+var matrix = [];
+var settings = {
+    rows: 8,
+    columns: 8
+};
 
 function generateEmptyTable() {
     var list = [];
-    for (var i = 0; i < 8; i++) {
+    for (var i = 0; i < settings.rows; i++) {
         list.push('<tr>');
-        for (var j = 0; j < 8; j++) {
+        for (var j = 0; j < settings.columns; j++) {
             var colorata = i % 2 ? j % 2 : ((j + 1) % 2);
             list.push('<td class="square ' + (colorata ? 'color' : '') + ' "></td>');
         }
@@ -29,14 +35,15 @@ function load() {
         dataType: 'json'
     }).done(function (raspuns) {
         console.debug('contacts loaded', raspuns);
-        var status = raspuns.table;
+        matrix = raspuns.table;
         nextPlayer = raspuns.nextPlayer;
-        for (var i = 0; i < 8; i++) {
-            for (var j = 0; j < 8; j++) {
-                var piesa = status [i][j];
-                if (piesa){
+        settings = raspuns.settings;
+        for (var i = 0; i < settings.rows; i++) {
+            for (var j = 0; j < settings.columns; j++) {
+                var piesa = matrix[i][j];
+                if (piesa) {
                     var td = $('#mainBoard tr').eq(i).find('td').eq(j);
-                    td.append('<div class="piesa player' + piesa +'"></div>');
+                    td.append('<div class="piesa player' + piesa + '"></div>');
                 }
             }
         }
@@ -47,15 +54,64 @@ function load() {
 generateEmptyTable();
 load();
 
+function checkNeighbours(rowIndex, cellIndex) {
+    //direction top-left (-1, -1)
+    if (matrix[rowIndex - 1][cellIndex - 1] == (nextPlayer == 1 ? 2 : 1)) {
+        console.log("Found enemy on top left");
+    }
+
+    //direction top (-1, 0)
+    if (matrix[rowIndex - 1][cellIndex] == (nextPlayer == 1 ? 2 : 1)) {
+        console.log("Found enemy on top");
+    }
+
+    //direction top-right (-1, +1)
+    if (matrix[rowIndex - 1][cellIndex + 1] == (nextPlayer == 1 ? 2 : 1)) {
+        console.log("Found enemy on top right");
+    }
+
+    //direction right (0, +1)
+    if (matrix[rowIndex][cellIndex + 1] == (nextPlayer == 1 ? 2 : 1)) {
+        console.log("Found enemy on right");
+    }
+
+    //direction bottom-right (+1, +1)
+    if (matrix[rowIndex + 1][cellIndex + 1] == (nextPlayer == 1 ? 2 : 1)) {
+        console.log("Found enemy on bottom right");
+    }
+
+    //direction bottom (+1, 0)
+    if (matrix[rowIndex + 1][cellIndex] == (nextPlayer == 1 ? 2 : 1)) {
+        console.log("Found enemy on bottom");
+    }
+
+    //direction bottom-left (+1, -1)
+    if (matrix[rowIndex + 1][cellIndex - 1] == (nextPlayer == 1 ? 2 : 1)) {
+        console.log("Found enemy on bottom left");
+    }
+
+    //direction left (0, -1)
+    if (matrix[rowIndex][cellIndex - 1] == (nextPlayer == 1 ? 2 : 1)) {
+        console.log("Found enemy on left");
+    }
+    console.log("________________"); //just empty space to separate messages
+}
+
 $("#mainBoard").on("click", "td", function () {
     var td = $(this);
-    if (td.find('.piesa').length == 0) {
+    // nextPlayer = nextPlayer==1 ? 2 : 1;
+
+
+    if (td.find('.piesa').length == 0) { //if the clicked cell is empty then put a piece
         td.append('<div class="piesa player' + nextPlayer + '"></div>');
-        if (nextPlayer == 1) {
-            nextPlayer = 2
-        } else if (nextPlayer == 2) {
-            nextPlayer = 1
-        }
+        matrix[this.parentNode.rowIndex][this.cellIndex]= nextPlayer;
+
+        checkNeighbours(this.parentNode.rowIndex, this.cellIndex);
+
+        nextPlayer = nextPlayer == 1 ? 2 : 1;
+    } else { //if the clicked cell is busy
+        console.log("Cell Busy!");
     }
+
 });
 
